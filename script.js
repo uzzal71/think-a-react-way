a4a654870e7be69d95bccab3111547fa0ca26f27
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-14 20:40:40 +0600

    lesson

---
+++ script.js
@@ -0,0 +1,8 @@
+let number = 0;
+const button = document.querySelector("#button");
+const display = document.querySelector("#display");
+
+button.addEventListener("click", () => {
+    number++;
+    display.textContent = number;
+})
