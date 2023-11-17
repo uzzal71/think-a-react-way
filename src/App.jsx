3b859778a26af675761fa45542d18bc78e8e0f07
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-17 22:32:12 +0600

    react element of a javascript object

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import React from "react";
+
 function App() {
+  const element = React.createElement('h1', null, 'hello world');
+  console.log(element);
   return (
     <div>
       <h1>Hello world</h1>
