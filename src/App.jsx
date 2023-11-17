54b4e1882013088f4434ed6a71a1e320dae7fb2d
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-17 22:44:05 +0600

    react element of a javascript object

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 function App() {
-  const element = React.createElement('h1', null, 'hello world');
-  const element1 = <h1>Hello world</h1>;
-  console.log(element1);
+  // const element = React.createElement('h1', {className: 'heading'}, 'hello world');
+  // const element1 = <h1>Hello world</h1>;
+  // console.log(element1);
+  const index = 0;
+  const element = (
+    <h1 className="heading", indexTab={index}>
+      Hello world
+    </h1>
+  )
+  /*
+  element = {
+    type: 'h1',
+    props: {
+      className: 'heading',
+      tabIndex: 0,
+      children: 'hellow world'
+    }
+  }
+  */
+ console.log(element)
   return (
     <div>
       <h1>Hello world</h1>
