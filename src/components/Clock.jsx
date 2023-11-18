738253ded937136495dc9be4f5087ec404f2ae48
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-18 15:10:48 +0600

    Clock component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 class Clock extends React.Component {
+    /*
     constructor(props) {
         super(props);
         this.state = { date: new Date() };
     }
+    */
+    state = { date: new Date() }
 
     componentDidMount() {
         this.clockTimer = setInterval(() => this.tick(), 1000)
