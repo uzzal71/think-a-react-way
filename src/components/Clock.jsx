9c24f580f2a858aeb0b2d6cae38b29de62df4881
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-18 15:00:54 +0600

    Clock component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
     render() {
         return (
         <h1 className="hading">
-            <span>{this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
+            <span>{this.props.children} {this.state.date.toLocaleTimeString(this.props.locale)}</span>
         </h1>
         );
     }
