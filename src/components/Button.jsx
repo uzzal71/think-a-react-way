0d8b13f7467a1f1f2fb0763f355aaf2b4ef4a62d
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-18 19:22:22 +0600

    Clock component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class Button extends React.Component {
+    shouldComponentUpdate(nextProps) {
+        const { change: currentChange } = this.props;
+        const { change: nextChange } = nextProps;
+        if (currentChange === nextChange) return false;
+        else return false
+    }
+
     render() {
         console.log('button component rendering');
         const { change } = this.props;
