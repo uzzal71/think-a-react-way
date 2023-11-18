348a391492d7217178d41d7945e256a7db77e074
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-18 15:08:12 +0600

    Clock component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -7,11 +7,17 @@ class Clock extends React.Component {
     }
 
     componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                date: new Date(),
-            });
-        }, 1000)
+        this.clockTimer = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
     }
 
     render() {
