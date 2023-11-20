2be0b96fc9f1aeb741dba96a6a0a91735a196559
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-21 00:41:58 +0600

    Composition vs Inheritance

--- src/components/composition/Emoji.jsx
+++ src/components/composition/Emoji.jsx
@@ -4,6 +4,6 @@ export default class Emoji extends Component {
     addEmoji = (text, emoji) => `${emoji} ${text} ${emoji}`
 
     render() {
-        
+        return this.props.children({addEmoji: this.addEmoji});
     }
 }
