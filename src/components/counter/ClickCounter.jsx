759d7a8a43f75f5cd5f77f0ecb3fa4e8c9c6116c
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-26 18:42:12 +0600

    React Higher Order Components

--- src/components/counter/ClickCounter.jsx
+++ src/components/counter/ClickCounter.jsx
@@ -5,12 +5,16 @@ class ClickCounter extends React.Component {
         count: 0,
     };
 
+    incrementCount = () => {
+        this.setState((prevState) => ({count: prevState.count + 1}));
+    }
+
     render() {
         const { count } = this.state;
 
         return(
             <div>
-                <button type="button">Clicked { count } times</button>
+                <button type="button" onClick={this.incrementCount}>Clicked { count } times</button>
             </div>
         );
     }
