e41a768b0959f23312dd8a7b0c7f5db960b0e19e
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-27 21:23:50 +0600

    React Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import React from "react";
+import Section from "./components/Section";
 import ClickCounter from "./components/counterRenderProps/ClickCounter";
 import Counter from "./components/counterRenderProps/Counter";
-import HoverCounter from "./components/counterRenderProps/HoverCounter";
 
-function App() {
+class App extends React.Component {
+  state = {
+    theme: 'dark'
+  };
+
+  render() {
+    const { theme } = this.state;
+
     return (
       <div>
         <Counter render={(counter, incrementCounter) => <ClickCounter counter={counter} incrementCounter={incrementCounter}/>}/>
-        <Counter render={(counter, incrementCounter) => <HoverCounter counter={counter} incrementCounter={incrementCounter}/>}/>
+        <Section theme={theme}/>
       </div>
     );
+  }
 }
 
 export default App
