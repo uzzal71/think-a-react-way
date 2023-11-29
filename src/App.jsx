928e2920e8313b41d55ce4aad7a6e35a97734e2e
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-29 23:33:17 +0600

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
-import React from "react";
-import Section from "./components/Section";
-import ClickCounter from "./components/counterRenderProps/ClickCounter";
-import Counter from "./components/counterRenderProps/Counter";
-import ThemeContext from "./contexts/themeContext";
 
 class App extends React.Component {
-  state = {
-    theme: 'dark',
-    switchTheme: () => {
-      this.setState(({ theme })=> {
-        if (theme === 'dark') return {theme: 'light'}
-        else return {theme: 'dark'}
-      })
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <Counter render={(counter, incrementCounter) => <ClickCounter counter={counter} incrementCounter={incrementCounter}/>}/>
-        <ThemeContext.Provider value={this.state}><Section /></ThemeContext.Provider>
-      </div>
-    );
-  }
+  
 }
 
 export default App
