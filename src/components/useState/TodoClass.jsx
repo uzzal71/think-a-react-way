a7848a24d017edb384ab0c441a8a7307f70ac791
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-01 00:40:26 +0600

    React useState hook

---
+++ src/components/useState/TodoClass.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class TodoClass extends Component {
+    state = {
+        todo: '',
+        waring: null,
+    };
+    
+    render() {
+        return (
+        <div>TodoClass</div>
+        )
+    }
+}
