a28c2dbda5efe12f64a5dda599edfaaca85cd0e5
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-30 21:34:43 +0600

    use state

--- src/components/useState/Form.jsx
+++ src/components/useState/Form.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 const Form = () => {
     const [number, setNumber] = useState(0)
+    const incrementNumber = () => {
+        setNumber((number) => number + 1);
+    }
     return (
         <div>
             <h1>This section use for form!</h1>
