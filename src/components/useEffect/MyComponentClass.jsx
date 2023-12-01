2924d8e9d628516dd8f9ee93549d560a43633688
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-01 22:45:31 +0600

    react useState

--- src/components/useEffect/MyComponentClass.jsx
+++ src/components/useEffect/MyComponentClass.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 class MyComponentClass extends React.Component {
+    state = {
+        count: 0,
+        date: new Date(),
+    };
+
+    add = () => {
+        this.setState(({ count }) => ({
+            count: count + 1,
+        }))
+    };
+
     render() {
+        const { data } = this.state
         return (
             <div>
-                <p>Time: </p>
+                <p>Time: {date.toLocalTimeString()}</p>
                 <p>
                     <button type='button'>Click</button>
                 </p>
