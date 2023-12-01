76b6dea457eca7a1d809b5cf1d0d0da184f5c29e
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-01 23:06:36 +0600

    react useState

--- src/components/useEffect/MyComponent.jsx
+++ src/components/useEffect/MyComponent.jsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from 'react';
 
 const MyComponent = () => {
     const [count, setCount] = useState(0);
+    const [text, setText] = useState('');
     const [date, setDate] = useState(new Date());
-
-    const addClick = () => {
-        setCount((prevCount) => prevCount + 1);
-    }
 
     const tick = () => {
         setCount(new Date());
     }
 
     useEffect(() => {
+        console.log('updating document title');
         document.title = `Clicked ${count} times`;
-    }, []);
+    }, [count]);
+
+    const addClick = () => {
+        setCount((prevCount) => prevCount + 1);
+    }
 
     return (
         <div>
             <p>Time: {date.toLocaleDateString()}</p>
+            <p>
+                <input type='text' name='text' value={text} onChange={(e) => setText(e.target.value)}/>
+            </p>
             <p>
                 <button type='button' onClick={addClick}>Click</button>
             </p>
