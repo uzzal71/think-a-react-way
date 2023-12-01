9b8260ee3282d62f49d64b3aa156f0ea75f7e4e3
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-01 23:45:10 +0600

    react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import MyComponent from "./components/useEffect/MyComponent";
 
 const App = () => {
+  const [show, setShow] = useState(true);
+
   return (
     <div className="app">
-      <div>We shall learn useEffect() today</div>
-      <MyComponent/>
+      <div>{show && <MyComponent/>}</div>
+      <p>
+        <button type="button" onClick={() => setShow((presShow) => !presShow)}>{show ? 'Hide post' : 'Show post'}</button>
+      </p>
     </div>
   );
 }
