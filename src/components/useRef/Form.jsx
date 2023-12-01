27cdb72104ff1c614bc3fbf83cf97cacb98272ff
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-02 01:33:45 +0600

    React useCallback & useMemo hook

---
+++ src/components/useRef/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <p>
+                <input type='text' placeholder='enter something'/>
+            </p>
+        </div>
+    );
+};
+
+export default Form;
