792c46ec3648d60282d07f3fba767c8172a222d9
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-02 02:06:23 +0600

    React useRef hook

---
+++ src/components/useRef/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./components/useRef/Form";
+import Time from "./components/useRef/Time";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Form/>
+      <Time/>
+    </div>
+  );
+}
+
+export default App
