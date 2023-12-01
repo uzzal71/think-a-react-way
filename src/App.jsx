d0d092d19a20449c7d2ef2e7ab0352eb6aab5cf1
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-02 02:06:02 +0600

    React useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./components/useRef/Form";
+import Time from "./components/useRef/Time";
 
 const App = () => {
   return (
     <div className="app">
       <Form/>
+      <Time/>
     </div>
   );
 }
--- src/components/useRef/Time.jsx
+++ src/components/useRef/Time.jsx
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Time = () => {
     const [date, setDate] = useState(new Date());
+    const intervalRef = useRef();
 
     const tick = () => {
         setDate(new Date());
     }
 
     useEffect(() => {
-        const interval = setInterval(tick, 1000);
+        intervalRef.current = setInterval(tick, 1000);
 
         // do the cleanup - stop the timer
         return () => {
-            clearInterval(interval)
+            clearInterval(intervalRef.current)
         }
-    });
+    }, []);
 
     return (
         <div>
             <p>Time: {date.toLocaleTimeString()}</p>
+            <p>
+                <button type="button" onClick={() => clearInterval(intervalRef.current)}>Cleanup</button>
+            </p>
         </div>
     );
 };
